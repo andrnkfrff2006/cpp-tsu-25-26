3432154de7666c172514c45c8126ea11ee527ba7
andrnkfrff2006 <andreynikiforoff2006@gmail.com>
2025-09-22 23:44:01 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-int main() {
-  std::cout << 700 << std::endl;
-  return 0;
+using namespace std;
+
+int main() 
+{
+    int res = 0;
+    int a;
+    int pos;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+      cin >> a >> pos; 
+      res = res + a * pos;
+    }
+    cout << res;
+    return 0;
 }
